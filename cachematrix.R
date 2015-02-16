a26f8518cc834ab779248ddbbdde2be43bd90ed9
Charles Brown <charles@charlesbrown.com>
2015-02-16 15:12:55 -0800

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,79 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Put comments here that give an overall description of what your functions do
 
 ## Write a short comment describing this function
+# This function creates a list of matrix functions
+# $set(y)         : using the <<- operator, this function
+#                   sets the cached matrix and clears the
+#                   cached inverse of the matrix
+# $get()          : gets the matrix data
+# $setmatrixinv() : using the <<- operator, this function
+#                   sets (calculates) and caches the inverse of the matrix
+# $getmatrixinv() : gets the cached inverse of the matrix
+makeCacheMatrix <- function(x = matrix()) 
+{
+     matrixinv <- NULL
+     set <- function(y)
+     {
+          x <<- y
+          matrixinv <<- NULL
+     }
+     get <- function() x
+     setmatrixinv <- function(ginv) matrixinv <<- ginv
+     getmatrixinv <- function() matrixinv
+     list(set = set, get = get, setmatrixinv = setmatrixinv, getmatrixinv = getmatrixinv)
+}
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## Write a short comment describing this function
+# "ginv" from the MASS package is used instead of solve
+# because ginv creates a matrix pseudoinverse that works
+# for all matrices
+# 
+# This function uses the list of matrix functions from makeCacheMatrix to
+# to check for and return the cached inverse or 
+# to calculate and return the calculate inverse 
+# when the cached inverse is not available
+cacheSolve <- function(x, ...)
+{
+     ## Return a matrix that is the inverse of 'x'
+     matrixinv <- x$getmatrixinv()
+     if(!is.null(matrixinv))
+     {
+          message("getting cached data")
+          return(matrixinv)
+     }
+     data <- x$get()
+     matrixinv <- ginv(data, ...)
+     x$setmatrixinv(matrixinv)
+     matrixinv
 }
 
 
-## Write a short comment describing this function
+library("MASS")
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+x <-c(1:16)
+x <-matrix(x, nrow = 4, ncol = 4, byrow = FALSE)
+x
+## create a list of functions (special matrix) from x
+xx <-makeCacheMatrix(x)
+## run cacheSolve using xx to get the chache the inverse of x or calculate the invers of x
+cacheSolve(xx)
+## run cacheSolve using xx again to retreive the cached inverse of x, skipping the re-calculation
+cacheSolve(xx)
+## run $set() function to reset x and clear the cached inverser of x
+xx$set(x)
+## run cacheSolve using xx and this time the inverse of x is calculated
+cacheSolve(xx)
+
+x <-c(2,0,0,1)
+x <-matrix(x, nrow = 2, ncol = 2, byrow = TRUE)
+x
+## create a list of functions (special matrix) from x
+xx <-makeCacheMatrix(x)
+## run cacheSolve using xx to calculate and chache the inverse of x
+cacheSolve(xx)
+## run cacheSolve using xx again to retreive the cached inverse of x, skipping the re-calculation
+cacheSolve(xx)
+## run $set() function to reset x and clear the cached mean
+xx$set(x)
+## run cacheSolve using xx and this time the inverse of x is calculated
+cacheSolve(xx)
